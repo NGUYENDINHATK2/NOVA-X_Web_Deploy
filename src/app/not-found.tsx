9cb885ec240dc8a4194a6623d60013de5cdad70d
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-08-10 13:57:26 +0900

    Add 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,3 @@
-'use client';
-
 export default function Custom404() {
-    return <div className="h-screen w-screen flex flex-1 flex-col items-center justify-center p-36">
-        <h1 className="font-extrabold text-bgPrimarySolidFocus text-4xl">404 - Page Not Found</h1>
-        <p className="font-semibold text-fgNeutralSubtle text-xl">Sorry, the page you are looking for does not exist.</p>
-        <div className="flex flex-1 ">
-        </div>
-    </div>
+  return <h1>404 - Page Not Found</h1>
 }
