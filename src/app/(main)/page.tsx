51540efa477c35977600c9ad6e0f1cc10e54d80d
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-08-10 15:55:39 +0900

    Build metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,14 @@
 import { withErrorHandling } from '@/common/hocs';
 import HomePage from '@/modules/Home/page';
+import type { Metadata } from 'next';
+
+
+
+export const metadata: Metadata = {
+  title: 'Nova-X',
+  description: 'Wellcome to Nova-X, the next generation communication platform.',
+};
+
 
 
 function Home() {
