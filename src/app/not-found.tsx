287d218193794d11beee5d04b00a32aed44db3cd
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-08-10 13:42:48 +0900

    Add 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Lottie from "lottie-react";
+import Link from "next/link";
+
+export default function Custom404() {
+    return <div className="h-screen w-screen flex flex-1 flex-col items-center justify-center p-36">
+        <h1 className="font-extrabold text-bgPrimarySolidFocus text-4xl">404 - Page Not Found</h1>
+        <p className="font-semibold text-fgNeutralSubtle text-xl">Sorry, the page you are looking for does not exist.</p>
+        <div className="flex flex-1 ">
+        </div>
+    </div>
+}
--- src/common/components/GuildLayoutRoot/GuildLayoutRoot.tsx
+++ src/common/components/GuildLayoutRoot/GuildLayoutRoot.tsx
@@ -7,13 +7,13 @@ interface IGuildLayoutRootProps {
 
 const GuildLayoutRoot  = ({ children }: IGuildLayoutRootProps) => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-screen flex flex-col">
         <div className="w-full flex items-center justify-center p-2">
-            <span className="text-sm">Máy chủ 1</span>
+            <span className="text-sm">Guild 1</span>
         </div>
-        <div className="flex w-full">
+        <div className="flex w-full h-full gap-2">
             <GuildSidebar />
-            <div className="flex flex-1">
+            <div className="flex flex-1 border rounded-tl-md border-neutral-800 h-full overflow-y-auto">
                  {children}
             </div>
         </div>
